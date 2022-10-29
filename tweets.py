7b19a2c54ce3df8aead3493e3abe7ae2618e02f6
Mochamad Lutfi Ashari <45476198+Lutfiazhar000@users.noreply.github.com>
2022-10-29 12:31:30 +0700

    Add files via upload

---
+++ tweets.py
@@ -0,0 +1,32 @@
+# import packages
+import snscrape.modules.twitter as sntwitter
+import pandas as pd
+
+# query data from twitter 
+# query = "(#prayforkanjuruhan) lang:en since:2022-10-01"
+query = input("enter your keyword: ")
+tweets = []
+limit = int(input("set limit: "))
+
+# looping for every tweet
+try:
+    print("start Scraping...")
+    for tweet in sntwitter.TwitterSearchScraper(query).get_items():
+
+        if len(tweets) == limit:
+            break
+        else:
+            tweets.append([tweet.date, tweet.user.username, tweet.content])
+
+    df = pd.DataFrame(tweets, columns=['full_date', 'user', 'tweet'])
+
+except:
+    print("scraping failed")
+
+# display
+print(df)
+print('Finished')
+print("--------")
+
+# save dataframe as csv file
+df.to_csv('tweets.csv', index=False)
